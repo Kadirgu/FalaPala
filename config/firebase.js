4506fb3f430358b278b2437e4ec1067037443a78
Kadirgu <Kadirgu@users.noreply.github.com>
2022-08-03 17:19:04 +0200

    update 5.4

--- config/firebase.js
+++ config/firebase.js
@@ -1,8 +1,6 @@
 
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
 
 // Firebase config
 const firebaseConfig = {
@@ -21,9 +19,3 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service (db)
 export const db = getFirestore(app);
-
-// Get a reference to Firebase Cloud Storage service
-export const storage = getStorage(app);
-
-// Get a reference to the Firebase auth object
-export const auth = getAuth();
