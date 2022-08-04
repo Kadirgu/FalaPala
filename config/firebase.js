ed5e8cebbd920d8c6e6ddbbe5ebda228c81ba549
Kadirgu <Kadirgu@users.noreply.github.com>
2022-08-04 10:33:05 +0200

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Firebase config
 const firebaseConfig = {
-    apiKey: "AIzaSyDNHECUKbWKGL0shCgBK-HN7ry6At3o504",
-    authDomain: "falapala-chatapp.firebaseapp.com",
-    projectId: "falapala-chatapp",
-    storageBucket: "falapala-chatapp.appspot.com",
-    messagingSenderId: "45406076426",
-    appId: "1:45406076426:web:395609f08d1894e95ae9f3",
-    measurementId: "G-CLDRKZPVNR"
-  };
+  apiKey: "AIzaSyDNHECUKbWKGL0shCgBK-HN7ry6At3o504",
+  authDomain: "falapala-chatapp.firebaseapp.com",
+  projectId: "falapala-chatapp",
+  storageBucket: "falapala-chatapp.appspot.com",
+  messagingSenderId: "45406076426",
+  appId: "1:45406076426:web:395609f08d1894e95ae9f3",
+  measurementId: "G-CLDRKZPVNR"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
