da494667d6d643a3a0882e611e055e36330c6a27
Kadirgu <Kadirgu@users.noreply.github.com>
2022-08-25 17:52:34 +0200

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,19 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 
-// Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyDNHECUKbWKGL0shCgBK-HN7ry6At3o504",
-  authDomain: "falapala-chatapp.firebaseapp.com",
-  projectId: "falapala-chatapp",
-  storageBucket: "falapala-chatapp.appspot.com",
-  messagingSenderId: "45406076426",
-  appId: "1:45406076426:web:b8808cc072f194755ae9f3",
-  measurementId: "G-T83VWTTCJ8"
-};
+// // Firebase config
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDNHECUKbWKGL0shCgBK-HN7ry6At3o504",
+//   authDomain: "falapala-chatapp.firebaseapp.com",
+//   projectId: "falapala-chatapp",
+//   storageBucket: "falapala-chatapp.appspot.com",
+//   messagingSenderId: "45406076426",
+//   appId: "1:45406076426:web:b8808cc072f194755ae9f3",
+//   measurementId: "G-T83VWTTCJ8"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service (db)
-export const db = getFirestore(app);
+// // Initialize Cloud Firestore and get a reference to the service (db)
+// export const db = getFirestore(app);
